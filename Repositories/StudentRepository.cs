db77c68798ab61df3d07179eff76e1b5a80cf41a
princeprasad28 <princeprasad.1991@gmail.com>
2020-06-06 15:23:39 +0530

    Files and subfolders setup

---
+++ Repositories/StudentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentApiWithMySql.Repositories
+{
+    public class StudentRepository: IStudentRepository
+    {
+    }
+}
